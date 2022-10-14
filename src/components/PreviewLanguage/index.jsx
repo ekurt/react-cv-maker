83d8fd9528c8ecd561c4d4057eaceddb19f26ba2
Emre Kurt <emrekurtmf@gmail.com>
2022-10-15 01:22:32 +0300

    added preview language component

--- src/components/PreviewLanguage/index.jsx
+++ src/components/PreviewLanguage/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./index.module.css";
 
 export const PreviewLanguage = () => {
-	return (
-		<div>
-			PreviewLanguage
-		</div>
-	);
+  const { languages } = useSelector((state) => state.form);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(languages, null, 2)}</pre>
+    </div>
+  );
 };
