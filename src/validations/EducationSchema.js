0dd39b2ccd49bf38ed0bd24f143b8a1999d72b8d
Emre Kurt <emrekurtmf@gmail.com>
2022-10-19 18:26:59 +0300

    updated education form validation schema

--- src/validations/EducationSchema.js
+++ src/validations/EducationSchema.js
@@ -4,7 +4,7 @@ export const EducationSchema = Yup.object().shape({
   school: Yup.string().required(),
   subject: Yup.string().required(),
   city: Yup.string().required(),
-  degree: Yup.string().required(),
+  degree: Yup.string(),
   from: Yup.string().required(),
   to: Yup.string().required(),
 });
