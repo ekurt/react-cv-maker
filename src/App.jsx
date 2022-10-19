812b56ff22a9b2aab1e44d14a1c23d0983015788
Emre Kurt <emrekurtmf@gmail.com>
2022-10-19 12:38:17 +0300

    styled print screen margin

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,15 @@ function App() {
     content: () => printRef.current,
   });
 
+  const getPageMargins = () => {
+    return `@page { margin: 0 0 50px 0 !important; }`;
+  };
+
   return (
     <div className={styles.app}>
       <Form handlePrint={handlePrint} />
       <div className={styles.print}>
+        <style>{getPageMargins()}</style>
         <Preview ref={printRef} />
       </div>
     </div>
