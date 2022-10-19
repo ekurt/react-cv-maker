e8702c6a5be857a2eeb69da355dfd91a99600f15
Emre Kurt <emrekurtmf@gmail.com>
2022-10-19 13:43:56 +0300

    updated social form structure

--- src/components/FormSocial/index.jsx
+++ src/components/FormSocial/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import { Input, Button } from "../_form";
+import { Input, Button, Select } from "../_form";
 import styles from "./index.module.css";
 import { SocialSchema } from "../../validations";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,53 +10,61 @@ export const FormSocial = () => {
   const dispatch = useDispatch();
   const { social } = useSelector((state) => state.form);
 
+  const handleResetData = () => {
+    dispatch(setSocial([]));
+  };
+
   return (
     <div className={styles.social}>
       <h4>Social</h4>
       <Formik
         validationSchema={SocialSchema}
         initialValues={{
-          twitter: "",
-          github: "",
-          linkedin: "",
-          website: "",
+          name: "",
+          link: "",
         }}
         onSubmit={(values, actions) => {
-          dispatch(setSocial([{ ...values }, ...social]));
+          dispatch(setSocial([...social, { ...values }]));
           actions.resetForm();
         }}
       >
-        {({ handleSubmit, handleReset, handleChange, values }) => (
+        {({ handleSubmit, handleReset, handleChange, values, dirty }) => (
           <Form onSubmit={handleSubmit} autoComplete="off" role="presentation">
-            <Input
-              onChange={handleChange}
-              value={values.twitter}
-              name="twitter"
-              placeholder="Twitter Username"
+            <Select
+              label="Type"
+              name="name"
+              options={[
+                { key: "Twitter", value: "Twitter Username" },
+                { key: "Github", value: "Github Username" },
+                { key: "LinkedIn", value: "LinkedIn Username" },
+                { key: "Website", value: "Personal Website" },
+                { key: "URL", value: "External URL" },
+              ]}
             />
             <Input
               onChange={handleChange}
-              value={values.github}
-              name="github"
-              placeholder="Github Username"
+              value={values.link}
+              name="link"
+              placeholder="Username or URL Address"
             />
-            <Input
-              onChange={handleChange}
-              value={values.linkedin}
-              name="linkedin"
-              placeholder="LinkedIn Username"
-            />
-            <Input
-              onChange={handleChange}
-              value={values.website}
-              name="website"
-              placeholder="Website URL"
-            />
-            <Button type="submit" variant="info">
+            <Button type="submit" variant="info" disabled={!dirty}>
               ADD
             </Button>
-            <Button type="reset" variant="danger" onClick={handleReset}>
-              CLEAR
+            <Button
+              type="reset"
+              variant="danger"
+              onClick={handleReset}
+              disabled={!dirty}
+            >
+              CLEAR FORM
+            </Button>
+            <Button
+              type="button"
+              variant="danger"
+              onClick={handleResetData}
+              disabled={social.length === 0}
+            >
+              CLEAR DATA
             </Button>
           </Form>
         )}
--- src/validations/SocialSchema.js
+++ src/validations/SocialSchema.js
@@ -1,8 +1,6 @@
 import Yup from "./_validation";
 
 export const SocialSchema = Yup.object().shape({
-  github: Yup.string(),
-  twitter: Yup.string(),
-  linkedin: Yup.string(),
-  website: Yup.string(),
+  name: Yup.string().required(),
+  link: Yup.string().required(),
 });
