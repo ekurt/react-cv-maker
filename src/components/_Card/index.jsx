0a796689b2ced3778609b2aabbce003c17242803
Emre Kurt <emrekurtmf@gmail.com>
2022-10-23 13:34:57 +0300

    updated card component style

--- src/components/_Card/index.jsx
+++ src/components/_Card/index.jsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 
 export const Card = ({ title, children }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.card}>
       <h4>{title}</h4>
       {children}
     </div>
