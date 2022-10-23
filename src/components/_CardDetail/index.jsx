9ec7132f403a6c86e7c9e65a15716838d73b33bc
Emre Kurt <emrekurtmf@gmail.com>
2022-10-23 21:27:25 +0300

    integrated dnd for card detail component

--- src/components/_CardDetail/index.jsx
+++ src/components/_CardDetail/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaTimes } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import styles from "./index.module.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export const CardDetail = ({ data, setter, print }) => {
   const dispatch = useDispatch();
@@ -10,33 +11,68 @@ export const CardDetail = ({ data, setter, print }) => {
     dispatch(setter(data.filter((item) => item._id !== id)));
   };
 
+  const handleOnDrugEnd = (result) => {
+    if (!result.destination) return;
+    const items = Array.from(data);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    dispatch(setter(items));
+  };
+
   if (data.length !== 0) {
     return (
       <div className={styles.cardDetail}>
         <hr className={styles.hr} />
-        {React.Children.toArray(
-          data.map((item) => {
-            let result = "";
+        <DragDropContext onDragEnd={handleOnDrugEnd}>
+          <Droppable droppableId="details">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {React.Children.toArray(
+                  data.map((item, index) => {
+                    let result = "";
 
-            return (
-              <div className={styles.item}>
-                <div className={styles.title}>
-                  {React.Children.toArray(
-                    print.map((it) => {
-                      return <span>{item[it]}</span>;
-                    })
-                  )}
-                </div>
-                <FaTimes
-                  className={styles.icon}
-                  onClick={() => {
-                    handleRemoveItem(item._id);
-                  }}
-                />
+                    return (
+                      <Draggable
+                        key={`${item._id}`}
+                        draggableId={`${item._id}`}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                          >
+                            <div className={styles.item}>
+                              <div className={styles.title}>
+                                {React.Children.toArray(
+                                  print.map((it) => {
+                                    return <span>{item[it]}</span>;
+                                  })
+                                )}
+                              </div>
+                              <FaTimes
+                                className={styles.icon}
+                                onClick={() => {
+                                  handleRemoveItem(item._id);
+                                }}
+                              />
+                            </div>
+                          </div>
+                        )}
+                      </Draggable>
+                    );
+                  })
+                )}
+
+                {provided.placeholder}
               </div>
-            );
-          })
-        )}
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     );
   }
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </>
 );
