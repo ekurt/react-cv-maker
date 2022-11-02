62f971f53ca805e43f9193a73975df699f9fa56c
Emre Kurt <emrekurtmf@gmail.com>
2022-11-02 14:35:04 +0300

    added language store

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import formReducer from "./form";
 import siteReducer from "./site";
+import languageReducer from "./language";
 
 export default configureStore({
   reducer: {
     form: formReducer,
     site: siteReducer,
+    language: languageReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
