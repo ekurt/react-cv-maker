4514fd6b62596691e3986476bb0f078699ee2cae
Emre Kurt <emrekurtmf@gmail.com>
2022-11-02 14:32:57 +0300

    updated language schema

--- src/validations/LanguageSchema.js
+++ src/validations/LanguageSchema.js
@@ -2,5 +2,5 @@ import Yup from "./_validation";
 
 export const LanguageSchema = Yup.object().shape({
   name: Yup.string().required(),
-  level: Yup.string(),
+  level: Yup.string().required(),
 });
