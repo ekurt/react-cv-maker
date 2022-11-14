ec884cf5be11a5263fbddd63ec0f4e7990afd6f5
Emre Kurt <emrekurtmf@gmail.com>
2022-11-14 18:51:03 +0300

    added helmet tags

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import { useEffect, useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import { useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 
 function App() {
   const { language } = useSelector((state) => state.language);
@@ -25,6 +26,40 @@ function App() {
 
   return (
     <div className={styles.app}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>CV Maker App</title>
+        <meta
+          name="description"
+          content="Easy, Customizable, Responsive, Free CV Maker App."
+        />
+        <link rel="canonical" href="https://cv-maker.ekurt.dev/" />
+
+        <meta property="og:url" content="https://cv-maker.ekurt.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="CV Maker App" />
+        <meta
+          property="og:description"
+          content="Easy, Customizable, Responsive, Free CV Maker App."
+        />
+        <meta
+          property="og:image"
+          content="https://cv-maker.ekurt.dev/assets/images/cv-maker-app.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="https://cv-maker.ekurt.dev/" />
+        <meta name="twitter:creator" content="@ekurtdev" />
+        <meta name="twitter:title" content="CV Maker App" />
+        <meta
+          name="twitter:description"
+          content="Easy, Customizable, Responsive, Free CV Maker App."
+        />
+        <meta
+          name="twitter:image"
+          content="https://cv-maker.ekurt.dev/assets/images/cv-maker-app.png"
+        />
+      </Helmet>
       <Form handlePrint={handlePrint} />
       <div className={styles.print}>
         <style>{getPageMargins()}</style>
